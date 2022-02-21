bc6f2217988ffaf76d47d0e1372a628d0ccf4d12
CangyuanLi <everest229@gmail.com>
2022-02-20 23:38:19 -0500

    added "even_split". Removed "maxmod"

--- cutils.py
+++ cutils.py
@@ -1,11 +1,11 @@
 from collections.abc import Iterable, Sequence
 import math
 
-def contains(x: Iterable, elements) -> bool:
+def contains(x: Iterable, elements: Iterable) -> bool:
     return any(elem in x for elem in elements)
 
 def chunk_list(lst: list, n: int) -> list:
-    """Splits a list into n sized chunk
+    """Splits a list into n sized chunks
 
     Args:
         lst (list): a list
@@ -18,33 +18,26 @@ def chunk_list(lst: list, n: int) -> list:
     for i in range(0, len(lst), n):
         yield lst[i:i+n]
 
+def even_split(lst: list, n: int) -> list[list]:
+    """Breaks a list into n roughly equal parts
+
+    Args:
+        lst (list): List to split
+        n (int): Number of parts
+
+    Returns:
+        list: List of n lists
+    """
+    k, m = divmod(len(lst), n)
+
+    return [lst[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n)]
+
 def find_last_index(x: Sequence, target):
     for i in range(len(x) - 1, -1, -1):
         if x[i] == target:
             return i
 
     return None
-
-def maxmod(n: int, k: int) -> int:
-    """Find the divisor that maximizes
-    the remainder
-
-    Args:
-        n (int): numerator
-        k (int): denominator
-
-    Returns:
-        int: divisor that maximized n mod k 
-    """
-    max = 0
-    for i in range(1, k, -1):
-        xx = n % i
-        if max < xx:
-            max = xx
-        if i < max:
-            break
-
-    return max
 
 def get_factors(n):
     factors = set()
--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import cutils
 
 def main():
     print(cutils.get_factors(50))
+    print(cutils.maxmod(80, 80))
 
 if __name__ == "__main__":
     main()
