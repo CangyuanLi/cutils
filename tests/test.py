a1f09fb37cc87bbd6bc38a1a348aefc1539494a6
CangyuanLi <everest229@gmail.com>
2022-03-15 23:55:49 -0400

    testing setup

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,4 @@
-import src.cutils.cutils as cutils
+from cutils import cutils
 import time
 
 def main():
@@ -6,8 +6,7 @@ def main():
     # print(cutils.even_split([1, 2, 3, 4, 5], 900))
     # print(cutils.flatten([1, "a", [1, 2]]))
 
-    cutils.time_func(lambda: cutils.get_factors(500000), iterations=200_000)
-    print(cutils.get_factors(2.2))
+    cutils.time_func(lambda: cutils.get_factors(500000), iterations=100_000, warmups=3)
 
 if __name__ == "__main__":
     main()
